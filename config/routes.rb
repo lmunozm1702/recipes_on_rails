45315d610634a517edc49fa400a96481e770215a
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-13 17:28:49 -0300

    Set / to devise login page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
 end
