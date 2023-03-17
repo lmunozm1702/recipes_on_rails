cee72812ed93dcfeefcd4546f2753cd9bf5ab46d
Amanuel Galema <amexabee@yahoo.com>
2023-03-17 22:10:39 +0200

    Add spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'has many food' do
+      @user = User.new(email: 'a@a', password: '123456')
+      @food = Food.create(name: 'Doro', user_id: @user.id, unit: 'grams', quantity: 1)
+      expect(@food.user_id).to eq(@user.id)
+    end
+
+    it 'has many recipe' do
+      @user = User.new(email: 'a@a', password: '123456')
+      recipe = Recipe.new(name: 'a', user: @user)
+      recipe.user = @user
+      expect(recipe.user).to eq(@user)
+    end
+  end
 end
