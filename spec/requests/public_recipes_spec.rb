d3825bf751af660d83fb512db354b47cdd25dbeb
Amanuel Galema <amexabee@yahoo.com>
2023-03-17 11:18:06 +0200

    Add tests for recipes, public_recipes and recipe_foods controllers

--- spec/requests/public_recipes_spec.rb
+++ spec/requests/public_recipes_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'PublicRecipes', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/public_recipes/index'
-      expect(response).to have_http_status(:success)
+  describe 'when user is signed in' do
+    include Devise::Test::IntegrationHelpers
+    before do
+      @user = User.create(email: 'a@a', password: '123456')
+      sign_in @user
+    end
+  
+    describe 'GET /index' do
+      it 'returns http success' do
+        get '/public_recipes/index'
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,7 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'when user is signed in' do
+    include Devise::Test::IntegrationHelpers
+    before do
+      @user = User.create(email: 'a@a', password: '123456')
+      @recipe = Recipe.create(name: 'Doro', user: @user)
+      sign_in @user
+    end
+
+    describe 'GET /recipes' do
+      it 'returns http success' do
+        get '/recipes'
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET /recipes/:id' do
+      it 'returns http success' do
+        get "/recipes/#{@recipe.id}"
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET /recipes/new' do
+      it 'returns http success' do
+        get '/recipes/new'
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'POST /recipes' do
+      it 'creates a new recipe' do
+        post '/recipes', params: { recipe: { name: 'Shiro', user_id: @user.id } }
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'PATCH /recipes/:id' do
+      it 'updates an existing recipe' do
+        patch "/recipes/#{@recipe.id}", params: { recipe: { name: 'Shiro' } }
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'DELETE /recipes/:id' do
+      it 'deletes an existing recipe' do
+        delete "/recipes/#{@recipe.id}"
+        follow_redirect!
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
